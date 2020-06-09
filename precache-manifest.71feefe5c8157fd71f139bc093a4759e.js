8bc1ceb2517c6029ffa2b6d9f21bff4eaca14c6e
Paul Perez <colopaul@hotmail.com>
2020-06-09 14:44:59 -0400

    Updates

---
+++ precache-manifest.71feefe5c8157fd71f139bc093a4759e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b9c93920fd6d4124445fd984e7fd26c",
+    "url": "/PaulPerez2483/mons.git/index.html"
+  },
+  {
+    "revision": "9566909ba94eb3a6e63a",
+    "url": "/PaulPerez2483/mons.git/static/css/main.3ed76e3a.chunk.css"
+  },
+  {
+    "revision": "51fc3ecca2d61bd0e562",
+    "url": "/PaulPerez2483/mons.git/static/js/2.a31ece46.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/PaulPerez2483/mons.git/static/js/2.a31ece46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9566909ba94eb3a6e63a",
+    "url": "/PaulPerez2483/mons.git/static/js/main.45809e94.chunk.js"
+  },
+  {
+    "revision": "c65859e9417083b07a88",
+    "url": "/PaulPerez2483/mons.git/static/js/runtime-main.d7602b36.js"
+  }
+]);
